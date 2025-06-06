cf1a908db15d9b957e72194a4d599cfdc49e6fa9
Willydabou <willydabou@gmail.com>
2025-06-06 19:34:04 +0300

    V01

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ fetch('footer.html').then(response => response.text())
     .then(data =>{
         document.querySelector('.footer-container').innerHTML = data;
     });
+
+
+    document.getElementById("contactForm").addEventListener("submit", function(e) {
+        e.preventDefault(); 
+    
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const subject = document.getElementById("subject").value.trim();
+        const message = document.getElementById("message").value.trim();
+    
+        if (!email || !subject || !message) {
+          alert("Email, Subject and Message are required.");
+          return;
+        }
+    
+        const myEmail = "dabouwilly@gmail.com";
+        const mailtoLink = `mailto:${myEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent("From: " + name + "\n\n" + message)}&reply-to=${email}`;
+    
+        window.location.href = mailtoLink;
+      });
